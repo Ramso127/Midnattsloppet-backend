94ba760e1e0dcc622875f317cb30dacbccfa4d9f
noaAlk <noa.al-khameesi@hotmail.com>
2024-04-08 18:49:05 +0200

    Spring Init

---
+++ src/test/java/com/pvt/groupOne/GroupOneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pvt.groupOne;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GroupOneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
