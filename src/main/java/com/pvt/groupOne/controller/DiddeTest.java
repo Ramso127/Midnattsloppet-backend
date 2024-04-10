5ab29d9099424775d5aea99effea3253556c1f4b
Didrik Grahn <didrikgrahn@gmail.com>
2024-04-10 17:29:49 +0200

    Added 2 classes and 1 interface: DiddeTest for Spring Boot testing, User, representing a user and UserRepository (which isn't currently working as intended)

--- src/main/java/com/pvt/groupOne/controller/DiddeTest.java
+++ src/main/java/com/pvt/groupOne/controller/DiddeTest.java
@@ -1,20 +1,46 @@
 package com.pvt.groupOne.controller;
 
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.pvt.groupOne.repository.User;
+import com.pvt.groupOne.repository.UserRepository;
 
 
 @Controller
-@RequestMapping(path="/test")
+@RequestMapping(path = "/test")
 @CrossOrigin
 public class DiddeTest {
+
+    @Autowired
+    private UserRepository userRepository;
     
-    @GetMapping(value="/hello")
+    @GetMapping(value = "/hello")
     public @ResponseBody String testMethod() {
-        return "Hello this is a test";
+        return "Hello this is Didrik's test";
     }
+
+    @GetMapping(value = "/greet/{firstName}/{lastName}")
+    public @ResponseBody String greetUser(@PathVariable String firstName, @PathVariable String lastName){
+
+        return "Hello, " + firstName + " " + lastName + "!";
+    }
+
+    // @GetMapping(value = "/adduser/{username}/{password}")
+    // public @ResponseBody String addUser(@PathVariable String username, @PathVariable String password){
+    //     User newUser = new User();
+    //     newUser.setUserName(username);
+    //     newUser.setPassword(password);
+    //     userRepository.save(newUser);
+        
+    //     return "User " + username + " with password " + password + " has been added to the database.";
+    // }
     
 }
---
+++ src/main/java/com/pvt/groupOne/repository/User.java
@@ -0,0 +1,35 @@
+package com.pvt.groupOne.repository;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+    
+    private String userName;
+    private String password;
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    
+}
