3706f66b6124f572d2c3d354f441dee2cc985f86
Didrik Grahn <didrikgrahn@gmail.com>
2024-04-10 16:05:27 +0200

    Made a test class to see if Tomcat works

---
+++ src/main/java/com/pvt/groupOne/controller/DiddeTest.java
@@ -0,0 +1,21 @@
+package com.pvt.groupOne.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+
+@Controller
+@RequestMapping(path="/test")
+@CrossOrigin
+public class DiddeTest {
+    
+    @GetMapping(value="/hello")
+    public @ResponseBody String testMethod() {
+        return "Hello this is a test";
+    }
+    
+}
