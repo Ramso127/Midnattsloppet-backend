2de6c2b55cce024a1c1793837aeed4d0f17669d1
Wilhelm <Wilhelm.schlyter@gmail.com>
2024-04-11 22:14:16 +0200

    reverted changes

--- src/main/java/com/pvt/groupOne/controller/MainController.java
+++ src/main/java/com/pvt/groupOne/controller/MainController.java
@@ -2,24 +2,15 @@ package com.pvt.groupOne.controller;
 
 import com.pvt.groupOne.model.User;
 import com.pvt.groupOne.model.RunnerGroup;
-
-import java.net.URI;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.pvt.groupOne.repository.RunnerGroupRepository;
-
-import jakarta.servlet.http.HttpServletRequest;
-
 import com.pvt.groupOne.repository.AccountRepository;
 
 @Controller
@@ -43,19 +34,16 @@ public class MainController {
         return "Hello, " + firstName + " " + lastName + "!";
     }
 
-    @PostMapping(value = "/adduser/{username}/{password}/{email}")
-    public @ResponseBody ResponseEntity<User> addUser(@PathVariable String username, @PathVariable String password,
-            @PathVariable String email, HttpServletRequest request) {
+    @GetMapping(value = "/adduser/{username}/{password}/{email}")
+    public @ResponseBody String addUser(@PathVariable String username, @PathVariable String password,
+            @PathVariable String email) {
         User newUser = new User();
         newUser.setUserName(username);
         newUser.setPassword(password);
         newUser.setEmail(email);
         accountRepository.save(newUser);
-        URI location = ServletUriComponentsBuilder.fromRequestUri(request)
-                .path("/{id}")
-                .buildAndExpand(newUser.getId())
-                .toUri();
-        return ResponseEntity.created(location).body(newUser);
+
+        return "User " + username + " with password " + password + " has been added to the database.";
     }
 
     @GetMapping(value = "/addgroup/{groupName}/{groupType}")
