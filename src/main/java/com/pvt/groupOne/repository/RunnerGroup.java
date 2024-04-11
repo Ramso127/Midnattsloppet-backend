d792520713e218ca58fd57646defa555ec8a442d
noaAlk <noa.al-khameesi@hotmail.com>
2024-04-11 16:09:32 +0200

    Operation Fix Clusterfuck 11

--- src/main/java/com/pvt/groupOne/repository/RunnerGroup.java
+++ src/main/java/com/pvt/groupOne/repository/RunnerGroup.java
@@ -6,7 +6,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
 @Entity
-public class RunnerGroups {
+public class RunnerGroup {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
