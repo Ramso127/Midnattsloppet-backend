f1e6108d3801804df7ab15ac34203660a58dbbdd
Wilhelm <Wilhelm.schlyter@gmail.com>
2024-04-11 16:06:30 +0200

    change name to runnergroups

--- src/main/java/com/pvt/groupOne/repository/RunnerGroups.java
+++ src/main/java/com/pvt/groupOne/repository/RunnerGroups.java
@@ -6,7 +6,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
 @Entity
-public class RunnerGroup {
+public class RunnerGroups {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
