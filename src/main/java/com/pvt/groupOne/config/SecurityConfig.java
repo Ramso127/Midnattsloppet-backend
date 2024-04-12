d5d3a46aabaec4c73bb35910943b9e38db1a3916
Wilhelm <Wilhelm.schlyter@gmail.com>
2024-04-12 15:15:29 +0200

    trying security

---
+++ src/main/java/com/pvt/groupOne/config/SecurityConfig.java
@@ -0,0 +1,12 @@
+package com.pvt.groupOne.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+public class SecurityConfig {
+    @Bean
+    public PasswordEncoder encoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
