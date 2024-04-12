a85a6e9b6d445e6db639d1434fb1356cc12fc7bf
Didrik Grahn <didrikgrahn@gmail.com>
2024-04-12 17:15:52 +0200

    Changed Integer to String due to changing primary key in User

--- src/main/java/com/pvt/groupOne/repository/AccountRepository.java
+++ src/main/java/com/pvt/groupOne/repository/AccountRepository.java
@@ -3,7 +3,7 @@ package com.pvt.groupOne.repository;
 import com.pvt.groupOne.model.User;
 import org.springframework.data.repository.CrudRepository;
 
-public interface AccountRepository extends CrudRepository<User, Integer> {
+public interface AccountRepository extends CrudRepository<User, String> {
     boolean existsByUsername(String username);
 
     User findByUsername(String username);
