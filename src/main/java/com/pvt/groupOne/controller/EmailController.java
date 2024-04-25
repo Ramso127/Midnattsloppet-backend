fe4d13c17dc9a24a58299c8d078f3ba3547ff0b9
noaAlk <noa.al-khameesi@hotmail.com>
2024-04-25 10:53:30 +0200

    Changed annotations in EmailController

--- src/main/java/com/pvt/groupOne/controller/EmailController.java
+++ src/main/java/com/pvt/groupOne/controller/EmailController.java
@@ -1,14 +1,14 @@
 package com.pvt.groupOne.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
 
 import com.pvt.groupOne.Service.EmailService;
 
-@RestController
+@Controller
+@RequestMapping(path = "/email")
+@CrossOrigin
 public class EmailController {
 
     @Autowired
