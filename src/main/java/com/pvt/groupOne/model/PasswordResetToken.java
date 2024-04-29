561841396718c9e729ac1428fbac68859b4048d3
noaAlk <noa.al-khameesi@hotmail.com>
2024-04-29 02:36:30 +0200

    Preparing password reset 5

--- src/main/java/com/pvt/groupOne/model/PasswordResetToken.java
+++ src/main/java/com/pvt/groupOne/model/PasswordResetToken.java
@@ -17,7 +17,8 @@ public class PasswordResetToken {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
-    @OneToOne(targetEntity = User.class, fetch = jakarta.persistence.FetchType.EAGER)
+    @OneToOne(targetEntity = User.class, fetch = FetchType.EAGER)
+    @JoinColumn(name = "user_name")
     private User user;
 
     private Date expiryDate;
@@ -38,4 +39,27 @@ public class PasswordResetToken {
     }
 
 
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
 }
