4bfb749fbb613709fece3e8217f36ca788a5a4a0
wisch96 <152611501+wisch96@users.noreply.github.com>
2024-05-03 16:20:32 +0200

    fix

--- src/main/java/com/pvt/groupOne/model/GroupRequest.java
+++ src/main/java/com/pvt/groupOne/model/GroupRequest.java
@@ -2,15 +2,11 @@ package com.pvt.groupOne.model;
 
 public class GroupRequest {
     String teamname;
-    byte[] image;
+
     String username;
 
     public String getUser() {
         return username;
-    }
-
-    public byte[] getImage() {
-        return image;
     }
 
     public String getTeamName() {
