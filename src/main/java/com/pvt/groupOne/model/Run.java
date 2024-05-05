625c7768371b634787549792940469f3c45cf9d3
Omar Almassri <ramso1000000@gmail.com>
2024-05-05 16:02:38 +0200

    Added Run model-class

---
+++ src/main/java/com/pvt/groupOne/model/Run.java
@@ -0,0 +1,99 @@
+package com.pvt.groupOne.model;
+
+import java.time.LocalDate;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "run")
+public class Run {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private LocalDate date;
+
+    @Column(nullable = false)
+    private double totalDistance;  // Total distance in kilometers
+
+    @Column(nullable = false)
+    private String totalTime;  // Total time as String, can be in format HH:mm:ss
+
+    @Column(nullable = false)
+    private double averageSpeed;  // Average speed in km/h
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;  // Link to User entity
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public double getTotalDistance() {
+        return totalDistance;
+    }
+
+    public void setTotalDistance(double totalDistance) {
+        this.totalDistance = totalDistance;
+    }
+
+    public String getTotalTime() {
+        return totalTime;
+    }
+
+    public void setTotalTime(String totalTime) {
+        this.totalTime = totalTime;
+    }
+
+    public double getAverageSpeed() {
+        return averageSpeed;
+    }
+
+    public void setAverageSpeed(double averageSpeed) {
+        this.averageSpeed = averageSpeed;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+    
+    @Override
+    public String toString() {
+        return "Run{" +
+               "id=" + id +
+               ", date=" + date +
+               ", totalDistance=" + totalDistance +
+               ", totalTime='" + totalTime + '\'' +
+               ", averageSpeed=" + averageSpeed +
+               ", user=" + user +
+               '}';
+    }
+}
+
