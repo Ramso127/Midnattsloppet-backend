393da5794ec917fb2a93d9ef3976bdf66305e434
Didrik Grahn <didrikgrahn@gmail.com>
2024-05-07 17:25:25 +0200

    Changed primary key of StravaRun to a generated Integer

--- src/main/java/com/pvt/groupOne/model/StravaRun.java
+++ src/main/java/com/pvt/groupOne/model/StravaRun.java
@@ -1,14 +1,16 @@
 package com.pvt.groupOne.model;
 
-import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 
 @Entity
 public class StravaRun {
 
     @Id
-    @Column(name = "date")
+    @GeneratedValue
+    Integer id;
+
     String date;
     double distance;
     int elapsedTime;
