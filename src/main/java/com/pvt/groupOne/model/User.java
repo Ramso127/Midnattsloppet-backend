0cdb721de7af0f3eea3a7496892c2715e84c6250
wisch96 <152611501+wisch96@users.noreply.github.com>
2024-05-07 11:56:51 +0200

    fixed id in User

--- src/main/java/com/pvt/groupOne/model/User.java
+++ src/main/java/com/pvt/groupOne/model/User.java
@@ -5,6 +5,8 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
@@ -14,7 +16,10 @@ import jakarta.persistence.OneToOne;
 public class User {
 
     @Id
-    @Column(name = "user_name")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "user_name", unique = true)
     private String username;
 
     @OneToOne(mappedBy = "user")
