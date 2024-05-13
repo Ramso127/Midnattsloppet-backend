4098eb984952782b3c7df9471c90c048b6c373ba
noaAlk <noa.al-khameesi@hotmail.com>
2024-05-13 21:30:55 +0200

    Added a passwordReset.html and a WebRouterController for web routing.

--- src/main/java/com/pvt/groupOne/controller/WebRouterController.java
+++ src/main/java/com/pvt/groupOne/controller/WebRouterController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping(path = "/passwordReset")
+@RequestMapping(path = "/route")
 @CrossOrigin
 public class WebRouterController {
 
