481c0dd9dfd549c3691b805a84caddab0a44cbf8
Didrik Grahn <didrikgrahn@gmail.com>
2024-05-14 16:37:54 +0200

    Added functionality to be able to run methods by schedule

--- src/main/java/com/pvt/groupOne/GroupOneApplication.java
+++ src/main/java/com/pvt/groupOne/GroupOneApplication.java
@@ -8,8 +8,10 @@ import com.pvt.groupOne.repository.AccountRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class GroupOneApplication {
 
 	public static void main(String[] args) {
