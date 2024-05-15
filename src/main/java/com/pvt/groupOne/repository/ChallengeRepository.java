38bf230a7986b494e11b8d48498f8b6f3b3fe997
Didrik Grahn <didrikgrahn@gmail.com>
2024-05-15 11:40:52 +0200

    Added some methods to ChallengeRepository

--- src/main/java/com/pvt/groupOne/repository/ChallengeRepository.java
+++ src/main/java/com/pvt/groupOne/repository/ChallengeRepository.java
@@ -4,7 +4,9 @@ import org.springframework.data.repository.CrudRepository;
 
 import com.pvt.groupOne.model.Challenge;
 
-public interface ChallengeRepository extends CrudRepository<Challenge, String> {
-    Challenge findByTitle(String username);
+public interface ChallengeRepository extends CrudRepository<Challenge, Integer> {
+    Challenge findByID(Integer id);
+
+    Challenge findByTitle(String title);
 
 }
