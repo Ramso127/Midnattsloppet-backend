b5e3211fedda0f555e94c820fae0a4673fbe6dbc
noaAlk <noa.al-khameesi@hotmail.com>
2024-05-16 11:56:51 +0200

    Changes to make WeeklyLeaderboard work

--- src/main/java/com/pvt/groupOne/model/WeeklyLeaderboard.java
+++ src/main/java/com/pvt/groupOne/model/WeeklyLeaderboard.java
@@ -1,19 +1,54 @@
 package com.pvt.groupOne.model;
 
-import org.springframework.data.annotation.Id;
-
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
 
 @Entity
 public class WeeklyLeaderboard {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne
     private RunnerGroup group;
 
     private int challengeID;
     private double measuredStat;
 
+    // Getters and setters
+    public Long getId() {
+        return id;
+    }
 
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public RunnerGroup getGroup() {
+        return group;
+    }
+
+    public void setGroup(RunnerGroup group) {
+        this.group = group;
+    }
+
+    public int getChallengeID() {
+        return challengeID;
+    }
+
+    public void setChallengeID(int challengeID) {
+        this.challengeID = challengeID;
+    }
+
+    public double getMeasuredStat() {
+        return measuredStat;
+    }
+
+    public void setMeasuredStat(double measuredStat) {
+        this.measuredStat = measuredStat;
+    }
 }
