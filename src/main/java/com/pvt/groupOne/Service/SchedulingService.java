58a442d873a74c76ba9e689f96f3ce972a5f3cee
Didrik Grahn <didrikgrahn@gmail.com>
2024-05-17 12:19:01 +0200

    Added incrementChallenges to SchedulingService runTask

--- src/main/java/com/pvt/groupOne/Service/SchedulingService.java
+++ src/main/java/com/pvt/groupOne/Service/SchedulingService.java
@@ -5,6 +5,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+import com.pvt.groupOne.controller.ChallengeController;
 import com.pvt.groupOne.model.Run;
 import com.pvt.groupOne.model.User;
 import com.pvt.groupOne.repository.RunRepository;
@@ -19,10 +20,14 @@ public class SchedulingService {
     @Autowired
     private RunRepository runRepository;
 
-    // TEMPLATE Run every tuesday at 0 sec, 42 minutes, 16 hours (16:42)
+    private ChallengeController challengeController = new ChallengeController();
+
+    // TEMPLATE Run every monday at 0 sec, 0 minutes, 0 hours (00:00)
     @Scheduled(cron = "0 00 00 * * MON", zone = "Europe/Stockholm")
     public void runTask() {
+
         // Logic
+        challengeController.incrementChallenges();
         System.out.println("Task executed on " + LocalDateTime.now());
     }
 }
