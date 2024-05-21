6af038ba3bb6514465aa3addd52bca584e874fbf
Didrik Grahn <didrikgrahn@gmail.com>
2024-05-21 16:58:37 +0200

    Added method to simulate a new week

--- src/main/java/com/pvt/groupOne/controller/TestController.java
+++ src/main/java/com/pvt/groupOne/controller/TestController.java
@@ -26,6 +26,8 @@ import com.pvt.groupOne.model.User;
 import com.pvt.groupOne.repository.AccountRepository;
 import com.pvt.groupOne.repository.RunRepository;
 import com.pvt.groupOne.repository.RunnerGroupRepository;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @Controller
 @RequestMapping(path = "/testing")
@@ -287,6 +289,7 @@ public class TestController {
             accountRepository.save(user);
             return "Success!";
     }
+    
 
     public boolean removeUser(String username) {
         User user = accountRepository.findByUsername(username);
@@ -313,6 +316,16 @@ public class TestController {
         }
 
         return true;
+    }
+
+    @GetMapping(value = "/simulate-new-week")
+    public @ResponseBody String simulateNewWeek(){
+        ChallengeController challengeController = new ChallengeController();
+
+        runnerGroupService.assignPointsForCurrentChallenge();
+        challengeController.incrementChallenges();
+
+        return "Done.";
     }
 
 
